41394f76921869f8c0ecc643875f91745c200d46
Anne Marie Okyere Adjekum <aokyereadjekum@gmail.com>
2026-02-13 15:37:36 -0800

    final deploy attempt

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,5 @@
+
+
 import {useState} from "react";
 import Controls from "./components/Controls";
 import Plots from "./components/Plots";
--- frontend/src/components/DriverDashboard.js
+++ frontend/src/components/DriverDashboard.js
@@ -1,4 +1,5 @@
 import {useState} from "react"
+const API_BASE = "https://build4students.onrender.com";
 
 //import "./App.css";
 function DriverDashboard() {
